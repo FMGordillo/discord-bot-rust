bab312b43564ae55ea8a5c018930382e6616e11a
FMGordillo <facundomgordillo@gmail.com>
2022-09-24 11:19:41 +0200

    feat: first commit :tada:

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::env;
+
+use serenity::async_trait;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::CommandResult;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+    async fn message(&self, _ctx: Context, msg: Message) {
+        println!("{} said: {}", msg.author.name, msg.content);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // let prefix = "papurri";
+    // let framework = StandardFramework::new()
+    //     .configure(|c| c.prefix(prefix))
+    //     .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        // .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+    Ok(())
+}
